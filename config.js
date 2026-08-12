809daf000cfb17974d00f0d69c1764b4200906e1
Nordriken <martinnordrik@gmail.com>
2026-08-12 13:54:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,14 @@
 //  Bytt altså ut  null  med objektet fra Firebase.
 // ---------------------------------------------------------------
 
-export const firebaseConfig = null;
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2Rygl2lFTcHXfOrzlLCZX2zlR6XhHPbo",
+  authDomain: "bergen-roma.firebaseapp.com",
+  projectId: "bergen-roma",
+  storageBucket: "bergen-roma.firebasestorage.app",
+  messagingSenderId: "432977126270",
+  appId: "1:432977126270:web:b02ec5eff62b5125a9c92f"
+};
 
 /*  Slik ser det ut når det er fylt ut:
 
